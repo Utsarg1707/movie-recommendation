9b0319661c312643bfd412d6d09fc0cd7c31a45a
siddhardhan s <codewithsidfiles23@gmail.com>
2025-04-17 17:21:44 +0530

    movie recommend app

--- src/recommend.py
+++ src/recommend.py
@@ -23,23 +23,20 @@ except Exception as e:
 
 
 def recommend_movies(movie_name, top_n=5):
-    logging.info("🎵 Recommending songs for: '%s'", movie_name)
+    logging.info("🎬 Recommending movies for: '%s'", movie_name)
     idx = df[df['title'].str.lower() == movie_name.lower()].index
     if len(idx) == 0:
-        logging.warning("⚠️ movie not found in dataset.")
+        logging.warning("⚠️ Movie not found in dataset.")
         return None
     idx = idx[0]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:top_n + 1]
-    song_indices = [i[0] for i in sim_scores]
+    movie_indices = [i[0] for i in sim_scores]
     logging.info("✅ Top %d recommendations ready.", top_n)
     # Create DataFrame with clean serial numbers starting from 1
-    result_df = df[['title']].iloc[song_indices].reset_index(drop=True)
+    result_df = df[['title']].iloc[movie_indices].reset_index(drop=True)
     result_df.index = result_df.index + 1  # Start from 1 instead of 0
     result_df.index.name = "S.No."
 
     return result_df
 
-op = recommend_movies("Batman v Superman: Dawn of Justice")
-
-print(op)
